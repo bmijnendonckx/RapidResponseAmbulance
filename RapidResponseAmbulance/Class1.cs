d79863bd45f5efdbff52265efecc4982ae896d82
Billy Mijnendonckx <billy.mijnendonckx@student.kdg.be>
2017-03-20 22:49:27 +0100

    Add project files.

---
+++ RapidResponseAmbulance/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidResponseAmbulance
+{
+    public class Class1
+    {
+    }
+}
