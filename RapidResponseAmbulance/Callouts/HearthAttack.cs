76116689109ed41ba1010402f401a8b585ecf0bd
Billy Mijnendonckx <billy.mijnendonckx@student.kdg.be>
2017-03-20 23:05:21 +0100

    startup project

---
+++ RapidResponseAmbulance/Callouts/HearthAttack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting.Entities;
+
+namespace RapidResponseAmbulance.Callouts {
+    //Very High for Testing purposes
+    [CalloutInfo("HearthAttack", CalloutProbability.VeryHigh)]
+    public class HearthAttack : Callout {
+    }
+}
---
+++ RapidResponseAmbulance/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+
+
+namespace RapidResponseAmbulance
+{
+    public class Main:Plugin {
+        public override void Finally() {
+            Game.LogTrivial("Rapid Response Ambulance has been cleaned up.");
+        }
+
+        public override void Initialize() {
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+
+            Game.LogTrivial("Plugin Rapig Response Ambulance " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " has been initialised.");
+        }
+
+        private static void OnOnDutyStateChangedHandler(bool onDuty) {
+            if(onDuty)
+                RegisterCallouts();
+        }
+
+        private static void RegisterCallouts() {
+
+        }
+    }
+}
